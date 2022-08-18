9e948cfd40f947ae3b8f258a34a5d9ccaa9efade
Josh <jsbusiness305@gmail.com>
2022-08-17 22:53:17 -0400

    worked on the search bar in create playlist component

--- spotify-clone/src/Components/Account/Library/CreatePlaylist/CreatePlaylist.js
+++ spotify-clone/src/Components/Account/Library/CreatePlaylist/CreatePlaylist.js
@@ -1,38 +1,53 @@
-import {useState} from 'react'
+import { useState } from 'react'
 import Sidebar from '../../../Sidebar/Sidebar'
 import './CreatePlaylist.css';
 
 function CreatePlaylist() {
 
-    const [emptyPlaylist, setPlaylist] = useState(true);
+
+    const [userInput, setInput] = useState('');
+
+    const handleInput = (e) => {
+
+        setInput(e.target.value);
+    }
+
+    const handleClearInput = () => {
+        setInput('');
+    }
+
 
     return (
         <div className="layout">
-    <Sidebar />
-    <div className='create__playlist'>
-      <div className='create__playlist__top'>
-            <div className='playlist__image'>
-                <div><i class="fa-solid fa-music"></i></div>
+            <Sidebar />
+            <div className='create__playlist'>
+                <div className='create__playlist__top'>
+                    <div className='playlist__image'>
+                        <div><i className="fa-solid fa-music"></i></div>
+                    </div>
+
+                    <div className='playlist__description'>
+                        <h5>Public Playlist</h5>
+                        <h3 className='font__large'>My Playlist #1</h3>
+                        <span>username</span>
+                    </div>
+                </div>
+
+                <div className='create__playlist__middle'>
+                    <i className="fa-solid fa-user-plus"></i>
+                    <i className="fa-solid fa-ellipsis"></i>
+                </div>
+
+                <div className='create__playlist__bottom'>
+                    <h3>Lets find something for your playlist</h3>
+                    <div className="search__div__playlist">
+                        <i className="fa-solid fa-magnifying-glass"></i>
+                        <input type='text' placeholder='Search for songs or artists' value={userInput} onChange={handleInput} />
+                        <i className="fa-solid fa-x" onClick={handleClearInput}></i>
+                    </div>
+                </div>
             </div>
-
-            <div className='playlist__description'>
-                <h5>Public Playlist</h5>
-                <h3 className='font__large'>My Playlist #1</h3>
-                <span>username</span>
-            </div>
-      </div>
-
-      <div className='create__playlist__middle'>
-      <i class="fa-solid fa-user-plus"></i>
-      <i class="fa-solid fa-ellipsis"></i>
-      </div>
-
-      <div className='create__playlist__bottom'>
-         <h3>Lets find something for your playlist</h3>
-         <input type='text' />   
-      </div>
-    </div>
-  </div>
+        </div>
     )
 }
 
